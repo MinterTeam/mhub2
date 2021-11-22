0b050633dd24cbccc626c45c5138edfea2c41b77
Daniil Lashin <danil-lashin@yandex.ru>
2021-11-22 13:19:38 +0300

    Fixes

--- keys-generator/cmd/mhub-keys-generator/main.go
+++ keys-generator/cmd/mhub-keys-generator/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/status-im/keycard-go/hexutils"
 	"os"
+	"strconv"
 )
 
 const ethSignaturePrefix = "\x19Ethereum Signed Message:\n32"
@@ -18,9 +19,10 @@ const ethSignaturePrefix = "\x19Ethereum Signed Message:\n32"
 func main() {
 	app.SetAddressConfig()
 
-	if len(os.Args) == 4 && os.Args[1] == "make_delegate_sign" {
+	if len(os.Args) == 5 && os.Args[1] == "make_delegate_sign" {
 		ethPrivateKey := os.Args[2]
 		cosmosAddr := os.Args[3]
+		nonce, err := strconv.Atoi(os.Args[4])
 
 		valAddress, err := sdk.AccAddressFromBech32(cosmosAddr)
 		if err != nil {
@@ -28,7 +30,7 @@ func main() {
 		}
 		signMsgBz := app.MakeEncodingConfig().Marshaler.MustMarshal(&types.DelegateKeysSignMsg{
 			ValidatorAddress: sdk.ValAddress(valAddress).String(),
-			Nonce:            0,
+			Nonce:            uint64(nonce),
 		})
 		hash := append([]uint8(ethSignaturePrefix), crypto.Keccak256Hash(signMsgBz).Bytes()...)
 		sig, err := secp256k1.Sign(crypto.Keccak256Hash(hash).Bytes(), hexutils.HexToBytes(ethPrivateKey))
@@ -36,7 +38,7 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Printf("%x", sig)
+		fmt.Printf("0x%x\n", sig)
 
 		return
 	}
@@ -48,5 +50,5 @@ func main() {
 	publicKeyECDSA, _ := publicKey.(*ecdsa.PublicKey)
 	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
 	fmt.Printf("Ethereum address: %s\n", address)
-	fmt.Printf("Minter address: Mx%s\n", address[:2])
+	fmt.Printf("Minter address: Mx%s\n", address[2:])
 }
