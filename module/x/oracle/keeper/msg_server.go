1296462fd217b6e665d3fe2f4e84b939f151e226
Daniil Lashin <danil-lashin@yandex.ru>
2021-11-24 09:38:01 +0300

    Add bnb price and bsc gas price to required prices in oracle

--- module/x/oracle/keeper/msg_server.go
+++ module/x/oracle/keeper/msg_server.go
@@ -66,7 +66,7 @@ func (k msgServer) PriceClaim(c context.Context, msg *types.MsgPriceClaim) (*typ
 	}
 
 	tokenInfos := k.Mhub2keeper.GetTokenInfos(ctx)
-	requiredPrices := []string{"eth", "eth/gas"}
+	requiredPrices := []string{"eth", "eth/gas", "bnb", "bsc/gas"}
 	for _, coin := range tokenInfos.TokenInfos {
 		requiredPrices = append(requiredPrices, coin.Denom)
 	}
