121f5c5d691777ad5f951a52d75c57fe8799ced1
Daniil Lashin <danil-lashin@yandex.ru>
2021-12-02 16:33:13 +0300

    test

--- orchestrator/relayer/src/find_latest_valset.rs
+++ orchestrator/relayer/src/find_latest_valset.rs
@@ -17,7 +17,7 @@ pub async fn find_latest_valset(
     web3: &Web3,
     chain_id: String,
 ) -> Result<Valset, GravityError> {
-    const BLOCKS_TO_SEARCH: u128 = 5_000u128;
+    const BLOCKS_TO_SEARCH: u128 = 100u128;
     let latest_block = web3.eth_block_number().await?;
     let mut current_block: Uint256 = latest_block.clone();
 
