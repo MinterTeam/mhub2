c3a7657e28e668b42a61be819e47670d58238dee
Daniil Lashin <danil-lashin@yandex.ru>
2022-10-25 11:34:40 +0300

    Prettify

--- module/cmd/mhub2/cmd/monitor.go
+++ module/cmd/mhub2/cmd/monitor.go
@@ -214,7 +214,7 @@ func AddMonitorCmd() *cobra.Command {
 						alert = fmt.Sprintf("🔴️")
 						failuresLog = fmt.Sprintf("%s⚠️️ <b>%s</b> ", failuresLog, v.GetMoniker())
 					}
-					t = fmt.Sprintf("%s\n%s %s (%d HUB)", t, alert, v.GetMoniker(), v.BondedTokens().QuoRaw(1e18).Int64())
+					t = fmt.Sprintf("%s\n%s <b>%s</b> %d HUB", t, alert, v.GetMoniker(), v.BondedTokens().QuoRaw(1e18).Int64())
 
 					if valHasFailure[v.OperatorAddress] {
 						var nonceErrs []string
